13b4724d2a9f67eb7bf611a72a05d13ee9aba392
yakun <2447745157@qq.com>
2026-04-17 21:31:20 +0800

    main

--- mall-admin/src/main/java/com/macro/mall/MallAdminApplication.java
+++ mall-admin/src/main/java/com/macro/mall/MallAdminApplication.java
@@ -2,6 +2,8 @@ package com.macro.mall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 /**
  * 应用启动入口
@@ -11,5 +13,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MallAdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(MallAdminApplication.class, args);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(MallAdminApplication.class, args);
+        Environment env = applicationContext.getEnvironment();
+        String port = env.getProperty("server.port");
+        System.out.println("\n----------------------------------------------------------\n" +
+                "\t项目启动成功！地址如下:\n" +
+                "\tLocal: \t\thttp://localhost:" + port + "/\n" +
+                "\tSwagger文档: \thttp://localhost:" + port + "/swagger-ui.html\n" +
+                "----------------------------------------------------------");
     }
 }
