c5caa841f04593d17261ec8eb813abc6f3c01c09
yakun <2447745157@qq.com>
2026-04-18 00:38:31 +0800

    uuid

--- mall-admin/src/main/java/com/macro/mall/demo/service/impl/AdminServiceImpl.java
+++ mall-admin/src/main/java/com/macro/mall/demo/service/impl/AdminServiceImpl.java
@@ -4,13 +4,15 @@ import com.macro.mall.demo.dto.LoginDTO;
 import com.macro.mall.demo.service.IAdminService;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 public class AdminServiceImpl implements IAdminService {
 
     @Override
     public String login(LoginDTO loginDTO) {
         if(loginDTO.getUsername().equals("admin") && loginDTO.getPassword().equals("123456")){
-            return "token";
+            return UUID.randomUUID().toString();
         };
         return "error";
     }
